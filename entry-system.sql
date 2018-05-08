dafe3a508efb0e437b4acfee037f98e5bd076b15
b3n10 <francisben.lleve@gmail.com>
2018-05-08 14:40:14 +0800

    Add SQL for table groups

--- entry-system.sql
+++ entry-system.sql
@@ -6,12 +6,17 @@ CREATE DATABASE entry_system;
 USE entry_system;
 
 CREATE TABLE users (
-	id int(11) not null AUTO_INCREMENT PRIMARY KEY,
+	id int(5) not null AUTO_INCREMENT PRIMARY KEY,
 	username varchar(20) not null,
 	password varchar(64) not null,
 	salt varchar(32) not null,
 	name varchar(50) not null,
 	date_joined datetime not null,
-	user_group int(11) not null
+	user_group int(5) not null
 );
 
+CREATE TABLE groups (
+	id int(5) not null AUTO_INCREMENT PRIMARY KEY,
+	name varchar(20) not null,
+	permissions text
+);
