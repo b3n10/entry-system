4a03f57c47e7922abcc384cd9a4b80b87c1ad447
b3n10 <francisben.lleve@gmail.com>
2018-05-08 13:40:48 +0800

    Create users table

--- entry-system.sql
+++ entry-system.sql
@@ -5,4 +5,13 @@ CREATE DATABASE entry_system;
 
 USE entry_system;
 
+CREATE TABLE users (
+	id int(11) not null AUTO_INCREMENT PRIMARY KEY,
+	username varchar(20) not null,
+	password varchar(64) not null,
+	salt varchar(32) not null,
+	name varchar(50) not null,
+	date_joined datetime not null,
+	user_group int(11) not null
+);
 
