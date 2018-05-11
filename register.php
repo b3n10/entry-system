2fd7240551fba85a7efe4aa7402102e701dbd208
b3n10 <francisben.lleve@gmail.com>
2018-05-12 00:56:45 +0800

    Add register form

---
+++ register.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Register</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<form action="" method="POST">
+		<div class="field">
+			<label for="username">Username:</label>
+			<input type="text" name="username" id="username" autocomplete="off">
+		</div>
+		<div class="field">
+			<label for="password1">Password:</label>
+			<input type="password" name="password1" id="password1">
+		</div>
+		<div class="field">
+			<label for="password2">Repeat Password:</label>
+			<input type="password" name="password2" id="password2">
+		</div>
+		<div class="field">
+			<label for="name">Name:</label>
+			<input type="text" name="name" id="name">
+		</div>
+		<button type="submit">Register</button>
+	</form>
+</body>
+</html>
+
