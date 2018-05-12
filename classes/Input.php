03558daa678e09a3599e59cdb855e351fabd20b7
b3n10 <francisben.lleve@gmail.com>
2018-05-12 22:01:04 +0800

    Add get method: retrieve the  from POST or GET array

--- classes/Input.php
+++ classes/Input.php
@@ -14,4 +14,12 @@ class Input {
 			break;
 		}
 	}
+
+	public static function get($item) {
+		if (isset($_POST[$item])) {
+			return $_POST[$item];
+		} else if (isset($_GET[$item])) {
+			return $_GET[$item];
+		}
+	}
 }
