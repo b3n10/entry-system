5e8dc96d1b75121d88bb0ae809800c276b68c39c
b3n10 <francisben.lleve@gmail.com>
2018-05-14 22:41:59 +0800

    check method: loop through field names

--- classes/Validate.php
+++ classes/Validate.php
@@ -13,23 +13,8 @@ class Validate {
 		foreach ($items as $item => $rules) {
 			// $item is the field name
 			// $rules is an array of the field name
-			foreach ($rules as $rule => $ruleValue) {
-
-				$value = $source[$item];
-
-				if ($rule === "required" && empty($value)) {
-					$this->addError("{$item} is required");
-				}
-
-			}
 		}
 
-		if (empty($this->_errors)) {
-			// if _errors array has no value, meaning no field name is added on the array or not blank
-			$this->_passed = true;
-		}
-
-		return $this;
 	}
 
 	public function addError($error) {
