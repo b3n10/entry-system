a6f154d388ae7e9fb90dc1a230da35bd5911b1d6
b3n10 <francisben.lleve@gmail.com>
2018-05-15 16:58:36 +0800

    Add exists method

--- classes/Session.php
+++ classes/Session.php
@@ -1,6 +1,10 @@
 <?php
 
 class Session {
+	public static function exists($token_name) {
+		return isset($_SESSION[$token_name]);
+	}
+
 	public static function put($name, $value) {
 		// add $name with $value to sessions global variable
 		return $_SESSION[$name] = $value;
