963762c2330566a026b95e1c3795f777cdbd006f
b3n10 <francisben.lleve@gmail.com>
2018-05-15 17:01:38 +0800

    Add delete method

--- classes/Session.php
+++ classes/Session.php
@@ -5,8 +5,15 @@ class Session {
 		return isset($_SESSION[$token_name]);
 	}
 
+	// add $name with $value to sessions global variable
 	public static function put($name, $value) {
-		// add $name with $value to sessions global variable
 		return $_SESSION[$name] = $value;
+	}
+
+	// check if exists then unset session variable
+	public static function delete($token_name) {
+		if (self::exists($token_name)) {
+			unset($_SESSION[$token_name]);
+		}
 	}
 }
