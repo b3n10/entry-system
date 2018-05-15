70251a26e94eb1095769d7f9838f149d0fccbf80
b3n10 <francisben.lleve@gmail.com>
2018-05-15 16:59:05 +0800

    Add check method

--- classes/Token.php
+++ classes/Token.php
@@ -5,4 +5,17 @@ class Token {
 		// generate a unique ID and use it for session variable
 		return Session::put(Config::get('session/token_name'), md5(uniqid()));
 	}
+
+	// check if $token passed exists then delete
+	// return true if success
+	public static function check($token) {
+		$token_name = Config::get('session/token_name');
+
+		if (Session::exists($token_name) && $token === Session::get($token_name)) {
+			Session::delete($token_name);
+			return true;
+		}
+
+		return false;
+	}
 }
