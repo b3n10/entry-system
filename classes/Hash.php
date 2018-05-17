da83c608d87ed840cb8f23430c88be81e6f7ca49
b3n10 <francisben.lleve@gmail.com>
2018-05-17 08:05:51 +0800

    Add make method

---
+++ classes/Hash.php
@@ -0,0 +1,12 @@
+<?php
+
+class Hash {
+	public static function make($string, $salt = '') {
+		// adds a random generated string to the password
+		// 'password = '12345'
+		// adding a random word on the password will change it's hash
+		// 'passwordDFDRERERR' = '888'
+		// use sha256 then pass the $string concatenated with $salt
+		return hash('sha256', $string . $salt);
+	}
+}
