507818234b27676ab0921da560962a247321ff11
b3n10 <francisben.lleve@gmail.com>
2018-05-21 06:47:17 +0800

    Add links

--- index.php
+++ index.php
@@ -4,3 +4,16 @@ require_once "core/init.php";
 if (Session::exists('home')) {
 	echo '<p>' . Session::flash('home') . '</p>';
 }
+
+$user = new User();
+
+if ($user->isLoggedIn()) {
+
+	echo 'Hi, ' . escape($user->data()->username);
+	echo '<br><a href="logout.php">Log out</a>.';
+
+} else {
+
+	echo 'Please <a href="login.php">log in</a> or <a href="register.php">register</a>.';
+
+}
