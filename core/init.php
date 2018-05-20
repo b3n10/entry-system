35c5bc24c50d7c4137ee7b09f65a84e6ffbf23ae
b3n10 <francisben.lleve@gmail.com>
2018-05-21 06:46:29 +0800

    Log user in if cookie exists even without session

--- core/init.php
+++ core/init.php
@@ -30,3 +30,19 @@ spl_autoload_register(function($class) {
 
 // include functions in functions directory
 require_once "functions/sanitize.php";
+
+// if cookie is set but session is not
+// e.g. if user closed the browser but checked the 'remember me' box
+if (Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))) {
+
+	// get cookie value for 'hash'
+	$hash = Cookie::get(Config::get('remember/cookie_name'));
+
+	// check if hash value is stored in record
+	$hash_check = DB::getInstance()->get('users_session', array('hash', '=', $hash));
+
+	// if hash value matches on record
+	if ($hash_check->count()) {
+		echo 'user should be logged in.';
+	}
+}
