0dfcf3133be23f74a61d66c35cb9ecd00f4e326d
b3n10 <francisben.lleve@gmail.com>
2018-05-20 19:53:24 +0800

    Init put method

--- classes/Cookie.php
+++ classes/Cookie.php
@@ -2,12 +2,23 @@
 
 class Cookie {
 
+	// Cookie::exists(cookie_name)
 	public static function exists($name) {
 		return (isset($_COOKIE[$name])) ? true : false;
 	}
 
+	// Cookie::get(cookie_name)
 	public static function get($name) {
 		return $_COOKIE[$name];
 	}
 
+	// Cookie::put(cookie_name, value, expiration)
+	public static function put($name, $value, $expiry) {
+		// create cookie
+	}
+
+	// Cookie::delete(cookie_name)
+	public static function delete($name) {
+		// reset cookie expiration to negative value
+	}
 }
