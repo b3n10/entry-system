725233537986112d37890bcec6a9f76df1688d65
b3n10 <francisben.lleve@gmail.com>
2018-05-21 19:45:37 +0800

    Add form to update

--- update.php
+++ update.php
@@ -9,3 +9,24 @@ if (!$user->isLoggedIn()) {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Update User Info</title>
+</head>
+<body>
+	<form action="" method="POST">
+		<div class="field">
+			<label>Old Name:</label>
+			<span><?php echo escape($user->data()->name); ?></span>
+		</div>
+		<div class="field">
+			<label for="name">New Name:</label>
+			<input type="text" id="name" name="name">
+		</div>
+		<button type="submit">Update</button>
+		<input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+	</form>
+</body>
+</html>
