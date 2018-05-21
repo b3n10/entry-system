8b773723b92af0c6174ba2034fe8723783de4fe0
b3n10 <francisben.lleve@gmail.com>
2018-05-21 16:48:44 +0800

    Add comments

--- classes/Session.php
+++ classes/Session.php
@@ -1,19 +1,23 @@
 <?php
 
 class Session {
+	// Session::exists
 	public static function exists($token_name) {
 		return isset($_SESSION[$token_name]);
 	}
 
-	// add $name with $value to sessions global variable
+	// Session::put
+	// add $name with $value to sessions global variable (or create a session)
 	public static function put($name, $value) {
 		return $_SESSION[$name] = $value;
 	}
 
+	// Session::get
 	public static function get($token_name) {
 		return $_SESSION[$token_name];
 	}
 
+	// Session::delete
 	// check if exists then unset session variable
 	public static function delete($token_name) {
 		if (self::exists($token_name)) {
@@ -21,6 +25,7 @@ class Session {
 		}
 	}
 
+	// Session::flash
 	// show success msg after registration
 	public static function flash($token_name, $string = '') {
 		// if session exists, delete then return it
