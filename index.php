453eb0aec1a3ba4e35c2075a75ebb5d9b933b7c1
b3n10 <francisben.lleve@gmail.com>
2018-05-21 18:31:11 +0800

    Add HTML

--- index.php
+++ index.php
@@ -6,14 +6,27 @@ if (Session::exists('home')) {
 }
 
 $user = new User();
+$msg = '';
 
 if ($user->isLoggedIn()) {
 
-	echo 'Hi, ' . escape($user->data()->username);
-	echo '<br><a href="logout.php">Log out</a>.';
+	// greet user with logout option
+	$msg = 'Hi, ' . escape($user->data()->username) . '. <a href="logout.php">Log out</a>.';
 
 } else {
 
-	echo 'Please <a href="login.php">log in</a> or <a href="register.php">register</a>.';
+	// show login/register link
+	$msg = 'Please <a href="login.php">log in</a> or <a href="register.php">register</a>.';
 
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+</head>
+<body>
+	<p><?php echo $msg; ?></p>
+</body>
+</html>
