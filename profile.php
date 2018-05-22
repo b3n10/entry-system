781a690acd7f9fd15801ba561e14ddf575bea3d5
b3n10 <francisben.lleve@gmail.com>
2018-05-22 20:32:59 +0800

    check user in record, if none redirect to 404, otherwise show user info

--- profile.php
+++ profile.php
@@ -6,4 +6,18 @@ require_once 'core/init.php';
 if (!$username = Input::get('user')) {
 	// send back to home if $_GET['user'] is blank
 	Redirect::to('index.php');
+} else {
+
+	// pass $username to constructo to find user
+	$user = new User($username);
+
+	// check if user is on record
+	if (!$user->exists()) {
+		Redirect::to(404);
+	} else {
+		$data = $user->data();
+	}
+
+	echo '<h3>' . $data->username . '</h3>';
+	echo '<p>Full name: ' . $data->name . '</p>';
 }
