5f7ce19273497f56a98cb988dd31e9cf87d4c2cf
b3n10 <francisben.lleve@gmail.com>
2018-05-22 08:50:37 +0800

    Add html form

---
+++ changepassword.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Change Password</title>
+</head>
+<body>
+
+	<form action="" method="POST">
+
+		<div class="field">
+			<label for="password1">Password:</label>
+			<input type="password" name="password1">
+		</div>
+
+		<div class="field">
+			<label for="password2">Confirm Password:</label>
+			<input type="password" name="password2">
+		</div>
+
+		<button type="submit">Update</button>
+		or go back&nbsp;
+		<a href="index.php">Home</a>
+
+		<input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+
+	</form>
+
+</body>
+</html>
